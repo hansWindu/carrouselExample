bcb6ea5d91faa4d42ce9bc8ec1ab2f3ba6f3103e
Hans Chris <55974402+hansWindu@users.noreply.github.com>
2021-02-01 20:53:29 +0000

    Primer paso

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// variables
+const objetoImg = document.querySelector('#imagen')
+const botonAvanzar = document.querySelector('#avanzar')
+const botonRetroceder = document.querySelector('#retroceder')
+const imagenes = ['img/1.jpg', 'img/2.jfif', 'img/3.jfif']
+let pagina = 1;
+
+// functions
+
+function avanzarFoto() {
+    pagina = pagina + 1;
+    render();
+}
+
+function retrocederFoto() {
+    pagina = pagina - 1;
+    render();
+}
+
+function render() {
+    objetoImg.setAttribute('src', imagenes[pagina - 1])
+}
+
+// eventos
+botonAvanzar.addEventListener('click', avanzarFoto)
+botonRetroceder.addEventListener('click', retrocederFoto)
+
+// inicio
+render();
